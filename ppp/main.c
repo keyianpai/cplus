a9d2d57dd0cf2c41c57de99bf3c2765a5dd9d4d0
ke <2995452226@qq.com>
2019-01-13 20:10:00 +0800

    Update main.c
    hiahiahia

--- ppp/main.c
+++ ppp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 void print_permutation(int n,int* A,int cur){
@@ -35,9 +36,37 @@ void print_permutation(int n,int* A,int cur){
 
     }
 }
+void str_period(char* s){
+    int len;
+    len = strlen(s);
+    printf("len in func is %d\n",len);
+for(int i=1;i<=len;i++){
+    if(len%i == 0){
+        int ok = 1;
+        for(int j=i;j<len;j++){
+            if(s[j]!=s[j%i]){
+                ok = 0;
+                break;
+
+            }
+        }
+        if(ok){
+            printf("%d",i);
+            break;
+        }
+    }
+
+}
+}
  int main(){
      int A[10];
-     print_permutation(4,A,0);
+     //print_permutation(4,A,0);
+     //char s[100]= {'q','q',"q","q","q",'3'};//"sssssss";
+     //char* s = "jjjjjjjj";
+     //char* s = "hiahiahia";
+     char* s = "hiahiahii";
+     printf("%d\n",strlen(s));
+     str_period(s);
 
 
  }
